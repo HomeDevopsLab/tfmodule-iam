5c8c9563e31a1c1ae5544075f63b87905cf68322
Krzysztof Królikowski <kkrolikowski@gmail.com>
2024-11-10 22:51:50 +0100

    feat(user): add change password permission

--- user/main.tf
+++ user/main.tf
@@ -5,3 +5,22 @@ resource "aws_iam_user_login_profile" "this" {
   user = aws_iam_user.this.name
   password_reset_required = true
 }
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = [ "iam:ChangePassword" ]
+    resources = [ aws_iam_user.this.arn ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name        = "ChangePassword"
+  description = "Allow users to change their own password"
+  policy      = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_user_policy_attachment" "this" {
+  user       = aws_iam_user.this.name
+  policy_arn = aws_iam_policy.this.arn
+}
