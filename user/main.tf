1090a7fa4f7d06a5db5fde61e2fed749ca198e0c
Krzysztof Królikowski <kkrolikowski@gmail.com>
2025-02-12 17:48:42 +0100

    feat: adding access key resource

--- user/main.tf
+++ user/main.tf
@@ -3,32 +3,33 @@ resource "aws_iam_user" "this" {
 }
 resource "aws_iam_user_login_profile" "this" {
   user = aws_iam_user.this.name
-  password_reset_required = true
+  password_reset_required = var.password_reset_required
 }
 
-data "aws_iam_policy_document" "change_password" {
+data "aws_iam_policy_document" "this" {
+  for_each = var.custom_policy
   statement {
-    actions = [ "iam:ChangePassword" ]
-    resources = [ aws_iam_user.this.arn ]
-    effect = "Allow"
-  }
-}
-
-data "aws_iam_policy_document" "full_access" {
-  statement {
-    actions = [ "*" ]
-    resources = [ "*" ]
-    effect = "Allow"
+    actions = each.value.actions
+    resources = each.value.resources
+    effect = each.value.effect
+    sid = "custom-policy-${each.key}"
   }
 }
 
 resource "aws_iam_policy" "this" {
+  for_each = var.custom_policy
   name        = "UserPolicy"
   description = "User access policy"
-  policy      = var.full_access ? data.aws_iam_policy_document.full_access.json : data.aws_iam_policy_document.change_password.json
+  policy      = data.aws_iam_policy_document.this[each.key].json
 }
 
 resource "aws_iam_user_policy_attachment" "this" {
+  for_each = var.custom_policy
   user       = aws_iam_user.this.name
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this[each.key].arn
+}
+
+resource "aws_iam_access_key" "this" {
+  count = var.access_key ? 1 : 0
+  user = aws_iam_user.this.name
 }
--- user/variables.tf
+++ user/variables.tf
@@ -2,7 +2,24 @@ variable "name" {
   type = string
 }
 
-variable "full_access" {
+variable "password_reset_required" {
+  description = "value to give console access to the user"
+  type    = bool
+  default = false
+}
+
+variable "custom_policy" {
+  description = "Custom policy to attach to the user"
+  type    = list(map({
+    actions   = list(string)
+    resources = list(string)
+    effect    = string
+  }))
+  default = []
+}
+
+variable "access_key" {
+  description = "Create access key for the user"
   type    = bool
   default = false
 }
