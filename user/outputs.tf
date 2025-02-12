cb9874f0a73c67967ffa6c14a5933afaf5beedae
Krzysztof Królikowski <kkrolikowski@gmail.com>
2025-02-12 17:57:49 +0100

    missing output

--- user/outputs.tf
+++ user/outputs.tf
@@ -2,3 +2,7 @@ output "password" {
   value     = aws_iam_user_login_profile.this.password
   sensitive = true
 }
+output "secret" {
+  value     = aws_iam_access_key.this[0].secret
+  sensitive = true
+}
